bf2ad4cd001e868dd95925842c207338e931f090
Felipe de Morais <felipejpa15@gmail.com>
2013-11-07 16:19:13 -0200

    crawler remove words with less than 3 chars, remove punctuation and tell wich word most apear in the text.

--- crawler.py
+++ crawler.py
@@ -1,6 +1,31 @@
+# _*_ encoding: utf-8 _*_
+
 import string
 import pdb
 # texto = "como uma onde no mar, como uma onda no mar."
+
+
+def count_a_number_of_repetitions_and_say_who_is_more_mentioned(txt):
+	txt = txt.split() 
+	counter_words = {}
+	# Separa as palavra mais mencionadas e quantas vezes foram mencionadas em um dicionário
+	for x in txt:
+		if x in counter_words:
+			counter_words[x] += 1
+		else:
+			counter_words[x] = 1
+
+	# Descobre quem é o mais mencionado
+	maxkey = ""	
+	maxval = 0		
+	for x in counter_words:
+		if counter_words[x] > maxval:
+			maxkey = x.title()
+			maxval = counter_words[x]
+
+	return string.lower(maxkey)
+	
+
 
 def remove_punctuation(txt):
 	return txt.translate(None, string.punctuation)
@@ -13,7 +38,15 @@ def remove_less_than_3(txt):
 		if len(txt[i]) < 3:
 			txt.remove(txt[i])
 		i += 1
+	# Transformando list em uma string separada por spaço
 	txt = string.join(txt, " ")
 	return txt
+
+def most_popular_word(txt):
+	txt_without_punctuation = remove_punctuation(txt)
+	txt_without_punctuation_and_less_than_3_words = remove_less_than_3(txt_without_punctuation)
+
+	return count_a_number_of_repetitions_and_say_who_is_more_mentioned(txt_without_punctuation_and_less_than_3_words)	
+
 
 
--- test_crawler.py
+++ test_crawler.py
@@ -1,3 +1,5 @@
+# _*_ encoding: utf-8 _*_
+
 import crawler
 import string 
 import unittest
@@ -5,19 +7,41 @@ import unittest
 class TestCrawler(unittest.TestCase): # remove punctuation
 
 	def test_remove_comma(self):
-		assert crawler.remove_punctuation("te,te") == "tete"
+		assert crawler.remove_punctuation("teste, teste") == "teste teste"
 
 	def test_remove_punctuation(self):
 		assert crawler.remove_punctuation("tete.") == "tete"
 
 	def test_remove_all_punctuation(self):
 		assert crawler.remove_punctuation("tete" + string.punctuation) == "tete"
+		assert crawler.remove_punctuation("ronaldo. ronaldo") == "ronaldo ronaldo"
+
 
 class TestReomveLessThan3(unittest.TestCase): # remove less than 3
+
 	def test_remove_less_than_3(self):
 		assert crawler.remove_less_than_3("tete de") == "tete"
 
+class count_words(unittest.TestCase):
+	
+	def test_count_words_an_tell_wich_most_apear(self):
+		assert crawler.count_a_number_of_repetitions_and_say_who_is_more_mentioned("ronaldo ronaldo") == "ronaldo"
 
+	def test_count_words_when_has_equal_mentions(self):
+		assert crawler.count_a_number_of_repetitions_and_say_who_is_more_mentioned("ronaldo  abc ronaldo abc") == "ronaldo"
 
+class TestPerfectWorld(unittest.TestCase):
+
+	def test_tell_the_most_popular_word_is_ronaldo(self):
+		assert crawler.most_popular_word("ronaldo ronaldo") == "ronaldo"
+
+	def test_tell_me_the_most_popular_word_is_ball(self):
+		assert crawler.most_popular_word("ball house ball") == "ball"
+
+	def test_tell_me_the_most_popular_word_is_ronaldo_removing_punctuation(self):
+		assert crawler.most_popular_word("ronaldo ronaldo, ronaldo. pelé pelé") == "ronaldo"
+	
+	def test_tell_me_the_most_popular_word_is_ronaldo_removing_punctuation_and_words_with_less_than_3_chars(self):	
+		assert crawler.most_popular_word("ronaldo de de de de de  ronaldo, de  ronaldo. de  pelé pelé") == "ronaldo"
 if __name__ == "__main__":
 	unittest.main()
